6ca7fd3a0877e4d67c78303368aac66a54aaaba5
fhfraga <f.henrique.fraga@gmail.com>
2020-10-02 17:18:02 -0300

    created alternative_names test and CO2 test

--- tests/test_data.py
+++ tests/test_data.py
@@ -10,6 +10,7 @@ def test_water():
     assert water_data.cas == '7732-18-5'
     assert water_data.formula == 'H2O'
     assert water_data.molar_mass == pint.Quantity(18.0153, 'gram/mole')
+    assert water_data.alternative_names == (None, None, None)
     assert isinstance(water_data.density_solid, pint.Quantity)
     assert water_data.density_solid == pint.Quantity(0.9167, 'gram/cm**3')
     assert water_data.density_liquid == pint.Quantity(0.9970474, 'gram/cm**3')
@@ -22,6 +23,28 @@ def test_water():
     assert water_data.enthalpy_sublimation == pint.Quantity(44.0, 'kJ/mole')
     assert water_data.enthalpy_vaporization == pint.Quantity(40.66, 'kJ/mole')
     assert water_data.volume_change_fusion == pint.Quantity(-1.634, 'cm**3/mole')
+
+def test_carbon_dioxide():
+    carbon_dioxide_data = PhaseDiagram('carbon dioxide')
+    assert isinstance(carbon_dioxide_data, PhaseDiagram)
+    assert carbon_dioxide_data.idx == 2
+    assert carbon_dioxide_data.name == 'carbon dioxide'
+    assert carbon_dioxide_data.cas == '124-38-9'
+    assert carbon_dioxide_data.formula == 'CO2'
+    assert carbon_dioxide_data.molar_mass == pint.Quantity(44.0095, 'gram/mole')
+    assert carbon_dioxide_data.alternative_names == ('carbonic anhydride', None, None)
+    assert isinstance(carbon_dioxide_data.density_solid, pint.Quantity)
+    assert carbon_dioxide_data.density_solid == pint.Quantity(1.562, 'gram/cm**3')
+    assert carbon_dioxide_data.density_liquid == pint.Quantity(1.179, 'gram/cm**3')
+    assert carbon_dioxide_data.antoine == (-56.57, 31.04, 7.58828, 861.8205, 271.88)
+    assert carbon_dioxide_data.boiling_point == (pint.Quantity(194.69, 'kelvin'), pint.Quantity(101325.0, 'pascal'))
+    assert carbon_dioxide_data.melting_point == (pint.Quantity(216.59, 'kelvin'), pint.Quantity(101325.0, 'pascal'))
+    assert carbon_dioxide_data.triple_point == (pint.Quantity(216.58, 'kelvin'), pint.Quantity(518500.0, 'pascal'))
+    assert carbon_dioxide_data.critical_point == (pint.Quantity(304.13, 'kelvin'), pint.Quantity(7.375E6, 'pascal'))
+    assert carbon_dioxide_data.enthalpy_fusion == pint.Quantity(9.02, 'kJ/mole')
+    assert carbon_dioxide_data.enthalpy_sublimation == pint.Quantity(25.2, 'kJ/mole')
+    assert carbon_dioxide_data.enthalpy_vaporization == pint.Quantity(15.8, 'kJ/mole')
+    assert carbon_dioxide_data.volume_change_fusion == pint.Quantity(-1.672, 'cm**3/mole')
 
 
 def test_state_index():
