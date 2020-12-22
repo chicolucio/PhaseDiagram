702205439bbd0fb3037d3b2b1ef0926a4fa9f551
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-12-22 17:55:25 -0300

    Mudando função para trabalhar apenas com as magnitudes

--- src/point_in_curve.py
+++ src/point_in_curve.py
@@ -6,4 +6,4 @@ def point_in_curve(point, tuples_of_points):
 
 
 def point_in_function(point, function, tolerance=0.001):
-    return np.isclose(point[1], function(point[0])[1], atol=tolerance)
+    return np.isclose(point[1].magnitude, function(point[0]).magnitude, atol=tolerance)
