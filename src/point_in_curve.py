42e540342bb2cdc7be5736457b25c012ee90b212
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2021-02-02 15:59:19 -0300

    Corrigindo função para ponto com base em testes com quantidades

--- src/point_in_curve.py
+++ src/point_in_curve.py
@@ -6,4 +6,4 @@ def point_in_curve(point, tuples_of_points):
 
 
 def point_in_function(point, function, tolerance=0.001):
-    return np.isclose(point[1].magnitude, function(point[0]).magnitude, atol=tolerance)
+    return np.isclose(point[1].magnitude, function(point[0])[1].magnitude, atol=tolerance)
--- tests/test_point_curve.py
+++ tests/test_point_curve.py
@@ -1,10 +1,14 @@
 from phase_diagram.phase_diagram import PhaseDiagram
 from src.point_in_curve import point_in_curve, point_in_function
 import numpy as np
+# from phase_diagram import ureg
 
 water = PhaseDiagram('H2O')
+ureg = water.ureg
+Q_ = ureg.Quantity
 
 
+@ureg.wraps((ureg.m, ureg.m), (ureg.m, ureg.m, None, ureg.m, ureg.m), strict=False)
 def straight_line(x0, range_=0, points=1, a=1, b=0):
     x = np.linspace(x0, x0 + range_, points)
     y = a * x + b
@@ -12,36 +16,36 @@ def straight_line(x0, range_=0, points=1, a=1, b=0):
 
 
 def test_point_in_straight_line_one_point():
-    assert point_in_curve((3, 3), straight_line(3))
+    assert point_in_curve((Q_('3 m'), Q_('3 m')), straight_line(3))
 
 
 def test_point_not_in_straight_line_one_point():
-    assert not point_in_curve((3, 4), straight_line(3))
+    assert not point_in_curve((Q_('3 m'), Q_('4 m')), straight_line(3))
 
 
 def test_point_in_straight_line_two_points():
-    assert point_in_curve((3, 3), straight_line(3, 4, 2))
+    assert point_in_curve((Q_('3 m'), Q_('3 m')), straight_line(3, 4, 2))
 
 
 def test_point_not_in_straight_line_two_points():
-    assert not point_in_curve((3, 4), straight_line(3, 4, 2))
+    assert not point_in_curve((Q_('3 m'), Q_('4 m')), straight_line(3, 4, 2))
 
 
 def test_point_in_straight_line_two_points_not_in_return_tuple():
-    assert not point_in_curve((3.1, 3.1), straight_line(3, 4, 2))
+    assert not point_in_curve((Q_('3.1 m'), Q_('3.1 m')), straight_line(3, 4, 2))
 
 
 def test_point_in_straight_line_function():
-    assert point_in_function((3, 3), straight_line)
+    assert point_in_function((Q_('3 m'), Q_('3 m')), straight_line)
 
 
 def test_point_not_in_straight_line_function():
-    assert not point_in_function((3, 4), straight_line)
+    assert not point_in_function((Q_('3 m'), Q_('4 m')), straight_line)
 
 
 def test_point_in_straight_line_function_tolerance():
-    assert point_in_function((3, 3.001), straight_line)
+    assert point_in_function((Q_('3 m'), Q_('3.001 m')), straight_line)
 
 
 def test_point_not_in_straight_line_function_tolerance():
-    assert not point_in_function((3, 3.002), straight_line)
+    assert not point_in_function((Q_('3 m'), Q_('3.002 m')), straight_line)
