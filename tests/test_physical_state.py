36de01eaef5bb17b57e487cde3e1f1b2d0844605
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2021-03-04 17:55:59 -0300

    Refactoring ureg in physical state tests

--- tests/test_physical_state.py
+++ tests/test_physical_state.py
@@ -1,66 +1,79 @@
 from phase_diagram.phase_diagram import PhaseDiagram
-from src.point_in_curve import point_in_function
-import numpy as np
+from phase_diagram.phase_diagram import ureg
+
+Q_ = ureg.Quantity
 
 water = PhaseDiagram('H2O')
-ureg_water = water.ureg
-Q_water = ureg_water.Quantity
-
 carbon_dioxide = PhaseDiagram('CO2')
-ureg_carbon = carbon_dioxide.ureg
-Q_carbon = ureg_carbon.Quantity
 
 
 def test_supercritical_fluid():
-    assert water.physical_state((Q_water('700 K'), Q_water('1e8 Pa'))) == 'supercritical fluid'
+    assert water.physical_state((Q_('700 K'), Q_('1e8 Pa'))) == 'supercritical fluid'
+
 
 def test_gas():
-    assert water.physical_state((Q_water('700 K'), Q_water('1e5 Pa'))) == 'gas'
+    assert water.physical_state((Q_('700 K'), Q_('1e5 Pa'))) == 'gas'
+
 
 def test_vapour_close_to_lv_curve():
-    assert water.physical_state((Q_water('500 K'), Q_water('1e5 Pa'))) == 'vapour'
+    assert water.physical_state((Q_('500 K'), Q_('1e5 Pa'))) == 'vapour'
+
 
 def test_vapour_close_to_sv_curve():
-    assert water.physical_state((Q_water('250 K'), Q_water('1e1 Pa'))) == 'vapour'
+    assert water.physical_state((Q_('250 K'), Q_('1e1 Pa'))) == 'vapour'
+
 
 def test_solid():
-    assert water.physical_state((Q_water('250 K'), Q_water('1e3 Pa'))) == 'solid'
+    assert water.physical_state((Q_('250 K'), Q_('1e3 Pa'))) == 'solid'
+
 
 def test_liquid():
-    assert water.physical_state((Q_water('400 K'), Q_water('1e7 Pa'))) == 'liquid'
+    assert water.physical_state((Q_('400 K'), Q_('1e7 Pa'))) == 'liquid'
+
 
 def test_supercritical_fluid_carbon_dioxide():
-    assert carbon_dioxide.physical_state((Q_carbon('350 K'), Q_carbon('1e10 Pa'))) == 'supercritical fluid'
+    assert carbon_dioxide.physical_state((Q_('350 K'), Q_('1e10 Pa'))) == 'supercritical fluid'
+
 
 def test_gas_carbon_dioxide():
-    assert carbon_dioxide.physical_state((Q_carbon('350 K'), Q_carbon('1e6 Pa'))) == 'gas'
+    assert carbon_dioxide.physical_state((Q_('350 K'), Q_('1e6 Pa'))) == 'gas'
+
 
 def test_vapour_close_to_lv_curve_carbon_dioxide():
-    assert carbon_dioxide.physical_state((Q_carbon('260 K'), Q_carbon('1e6 Pa'))) == 'vapour'
+    assert carbon_dioxide.physical_state((Q_('260 K'), Q_('1e6 Pa'))) == 'vapour'
+
 
 def test_vapour_close_to_sv_curve_carbon_dioxide():
-    assert carbon_dioxide.physical_state((Q_carbon('200 K'), Q_carbon('1e4 Pa'))) == 'vapour'
+    assert carbon_dioxide.physical_state((Q_('200 K'), Q_('1e4 Pa'))) == 'vapour'
+
 
 def test_solid_dioxide_carbon():
-    assert carbon_dioxide.physical_state((Q_carbon('180 K'), Q_carbon('1e6 Pa'))) == 'solid'
+    assert carbon_dioxide.physical_state((Q_('180 K'), Q_('1e6 Pa'))) == 'solid'
+
 
 def test_liquid_dioxide_carbon():
-    assert carbon_dioxide.physical_state((Q_carbon('240 K'), Q_carbon('1e7 Pa'))) == 'liquid'
+    assert carbon_dioxide.physical_state((Q_('240 K'), Q_('1e7 Pa'))) == 'liquid'
+
 
 def test_liquid_vapour_balance_water():
-    assert water.physical_state((Q_water('647.1 K'), Q_water('21936363.03431385 Pa'))) == 'liquid-vapour balance'
+    assert water.physical_state((Q_('647.1 K'), Q_('21936363.03431385 Pa'))) == 'liquid-vapour balance'
+
 
 def test_solid_liquid_balance_water():
-    assert water.physical_state((Q_water('268.16 K'), Q_water('70096120.44156641 Pa'))) == 'solid-liquid balance'
+    assert water.physical_state((Q_('268.16 K'), Q_('70096120.44156641 Pa'))) == 'solid-liquid balance'
+
 
 def test_solid_vapour_balance_water():
-    assert water.physical_state((Q_water('213.16000000000003 K'), Q_water('2.619617119846615 Pa'))) == 'solid-vapour balance'
+    assert water.physical_state((Q_('213.16000000000003 K'), Q_('2.619617119846615 Pa'))) == 'solid-vapour balance'
+
 
 def test_liquid_vapour_balance_dioxide_carbon():
-    assert carbon_dioxide.physical_state((Q_carbon('304.13 K'), Q_carbon('7371844.047303765 Pa'))) == 'liquid-vapour balance'
+    assert carbon_dioxide.physical_state((Q_('304.13 K'), Q_('7371844.047303765 Pa'))) == 'liquid-vapour balance'
+
 
 def test_solid_liquid_balance_dioxide_carbon():
-    assert carbon_dioxide.physical_state((Q_carbon('221.58 K'), Q_carbon('23011236.108132083 Pa'))) == 'solid-liquid balance'
+    assert carbon_dioxide.physical_state((Q_('221.58 K'), Q_('23011236.108132083 Pa'))) == 'solid-liquid balance'
+
 
 def test_solid_vapour_balance_dioxide_carbon():
-    assert carbon_dioxide.physical_state((Q_carbon('156.58 K'), Q_carbon('2.619617119846615 Pa'))) == 'solid-vapour balance'
+    assert carbon_dioxide.physical_state((Q_('156.58 K'), Q_('2.619617119846615 Pa'))) == 'solid-vapour balance'
--- tests/test_point_curve.py
+++ tests/test_point_curve.py
@@ -60,26 +60,34 @@ def test_point_in_straight_line_function_tolerance():
 def test_point_not_in_straight_line_function_tolerance():
     assert not point_in_function((Q_('3 m'), Q_('3.002 m')), straight_line_y)
 
+
 def test_point_water_01():
     assert point_in_function((Q_('400 K'),Q_('246493.814 Pa') ), water._antoine_lv)
+
 
 def test_point_water_02():
     assert not point_in_function((Q_('400 K'),Q_('246493.813 Pa') ), water._antoine_lv)
 
+
 def test_point_water_03():
     assert point_in_function((Q_('100 K'),Q_('1.64e-12 Pa')), water_clapeyron_sv)
+
 
 def test_point_water_04():
     assert not point_in_function((Q_('100 K'),Q_('1.64e-3 Pa')), water_clapeyron_sv)
 
+
 def test_point_water_05():
     assert point_in_function((Q_('200 K'),Q_('0.875 Pa')), water_clapeyron_lv)
+
 
 def test_point_water_06():
     assert not point_in_function((Q_('200 K'),Q_('0.877 Pa')), water_clapeyron_lv)
 
+
 def test_point_water_07():
     assert point_in_function((Q_('270 K'),Q_('44150144.527 Pa')), water._clapeyron_sl)
+
 
 def test_point_water_08():
     assert not point_in_function((Q_('270 K'),Q_('44150144.529 Pa')), water._clapeyron_sl)
