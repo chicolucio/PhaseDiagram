ddb9c6bb01c7bace76c51d36704a55fbb64c540c
Francisco Bustamante <chicolucio@gmail.com>
2021-04-25 14:01:23 -0300

    point_in_curve function removed. Tests clean up

--- src/point_in_curve.py
+++ src/point_in_curve.py
@@ -1,9 +1,18 @@
 import numpy as np
 
 
-def point_in_curve(point, tuples_of_points):
-    return point in zip(*tuples_of_points)
-
-
 def point_in_function(point, function, tolerance=0.001):
+    """
+    Verifies if a given point coordinates are in a given function domain/image
+
+    Parameters
+    ----------
+    point : tuple of pint quantities
+    function : function
+    tolerance : float, optional
+
+    Returns
+    -------
+    bool
+    """
     return np.isclose(point[1].magnitude, function(point[0]).magnitude, atol=tolerance, rtol=0)
